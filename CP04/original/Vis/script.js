83a1bf6d93b575176592288f31fc0c1727ad5fad
RicardoMartins55 <ricardo.martins.escola@gmail.com>
2022-10-22 18:40:18 +0100

    implemented matrix

--- CP04/original/Vis/script.js
+++ CP04/original/Vis/script.js
@@ -3,36 +3,118 @@ const width_left = 0.326 * window.innerWidth - margin.left - margin.right;
 const height_left_top = 0.563 * window.innerHeight - margin.top - margin.bottom;
 const height_left_bottom = 0.281 * window.innerHeight - margin.top - margin.bottom;
 
-const width_right = 0.651 * window.innerWidth - margin.left - margin.right;
+const width_right = 0.610 * window.innerWidth - margin.left - margin.right;
 const height_right = 0.478 * window.innerHeight - margin.top - margin.bottom;
 
 
-  function init() {
-  createBarChart("#vi1");
+function init() {
+  createHeatmap("#vi1");
   //createBarChart("#vi4");
   //createBarChart("#vi5");
   createParallelCoordinates("#vi2");
   createScatterPoltMoves("#vi3");
 }
 
-function createBarChart(id) {
+function createHeatmap(id) {
   const svg = d3
     .select(id)
     .attr("width", width_left + margin.left + margin.right)
     .attr("height", height_left_top + margin.top + margin.bottom)
     .append("g")
-    .attr("id", "gBarChart")
+    .attr("id", "gHeatmap")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
   
-  d3.json("data.json").then(function (data) {
-    const x = d3.scaleLinear().domain([0, 10]).range([0, width_left]);
+  d3.json("json/types_usage.json").then(function (data) {
+
+    const types = ["Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Ghost", "Steel", "Fire", "Water", "Grass",
+    "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy" ];
+
+    const x = d3.scaleBand()
+      .range([   0, width_left ])
+      .domain(types)
+    
+    const y = d3.scaleBand()
+      .range([height_left_top , 0 ])
+      .domain(types)
+
+    var myColor = d3.scaleSequential()
+      .interpolator(d3.interpolateRdYlGn)
+      .domain(d3.extent(data, d => d.Monthly_Usage))
+
+    // create a tooltip
+    var tooltip = d3.select("#vi1")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "3px")
+      .style("padding", "5px")
+
+    // Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(d) {
+      tooltip
+        .style("opacity", 1)
+      d3.select(this)
+        .style("stroke", "black")
+        .STYLE("stroke-width", 0.5)
+        .style("opacity", 1)
+    }
+    var mousemove = function(d) {
+      tooltip
+        .html("Monthly Usage of "+ d.Type1 + "-" + d.Type2 + " Pokemon: " + d.Monthly_Usage)
+        .style("left", (d3.mouse(this)[0]+70) + "px")
+        .style("top", (d3.mouse(this)[1]) + "px")
+    }
+    var mouseleave = function(d) {
+      tooltip
+        .style("opacity", 0)
+      d3.select(this)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+    }
+
     svg
       .append("g")
-      .attr("id", "gXAxis")
+      .attr("class", "gXAxis")
       .attr("transform", `translate(0, ${height_left_top})`)
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x).tickSize(0)) 
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-65)")
+        .select(".domain").remove()
+      
+    svg
+      .append("g")
+      .attr("id", "gYAxis")
+      .attr("transform", `translate(0, 0)`)
+      .call(d3.axisLeft(y).tickSize(0))
+      .select(".domain").remove()
 
-    const y = d3
+    svg.selectAll()
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.Type1) })
+      .attr("y", function(d) { return y(d.Type2) })
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .attr("width", x.bandwidth() )
+      .attr("height", y.bandwidth() )
+      .style("fill", function(d){return myColor(d.Monthly_Usage)} )
+      .style("stroke-width", 4)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave)
+    
+
+
+    /*const y = d3
       .scaleBand()
       .domain(data.map((d) => d.oscar_year))
       .range([0, height_left_top])
@@ -43,7 +125,7 @@ function createBarChart(id) {
       .domain([0, d3.max(data, (d) => d.budget)])
       .range([0, 1]);
 
-    svg.append("g").attr("id", "gYAxis").call(d3.axisLeft(y));
+    svg.append("g").attr("id", "gYAxis").call(d3.axisLeft(y));*/
   });
 }
 
@@ -60,8 +142,6 @@ function createParallelCoordinates(id) {
 
           
     const stats = ["Total", "HP", "Attack", "Defense", "Special_Atk", "Special_Def", "Speed"];
-
-    const stats_double = ["Total", "HP", "Attack", "Defense", "Special_Atk", "Special_Def", "Speed", "Speed", "Special_Def",  "Special_Atk","Defense","Attack","HP","Total"];
 
     function value(key, d){
       if(key == "Total" )
@@ -87,7 +167,7 @@ function createParallelCoordinates(id) {
                                    "#0E3289", "#5F902D", "#826904", "#950631", "#256363", "#3506A9", "#5A463A", "#691125"])
 
 
-    const x = d3.scalePoint(stats, [0, (6/7)* width_right]);
+    const x = d3.scalePoint(stats, [0, (19/20)* width_right]);
 
     const y = new Map(Array.from(stats, key => [key, d3.scaleLinear(d3.extent(data, d => value(key, d)), [10, height_right])]))
 
@@ -110,7 +190,7 @@ function createParallelCoordinates(id) {
       .attr("stroke", d => color(d.Type1))
       .attr("d", d => line(d3.cross([d], stats, (element, key) => [value(key, element), key])))
       .append("title")
-        .text(d => d.Pokemon);
+        .text((d) => d.Pokemon);
 
 
     svg.append("g")
